9b054f83b2a62f8be63f2e45ffb1e2425589c9c2
Nikolay Makarov <makarov-na@yandex.ru>
2019-11-03 20:55:19 +0300

    Исправил ошибки

--- install.sh
+++ install.sh
@@ -6,20 +6,26 @@ sudo apt-get --assume-yes install htop
 sudo apt-get --assume-yes install rsync
 sudo apt-get --assume-yes install transmission-daemon
 sudo service transmission-daemon stop
+sleep 10
 sudo cp -r /etc/transmission-daemon ~/.config/transmission-daemon
 sudo cp ./settings.json ~/.config/transmission-daemon
 sudo chown -R xbian ~/.config
+sudo chmod -R ug+rw ~/.config
 
-cat /etc/default/transmission-daemon | grep -v CONFIG_DIR | grep -v USER | sudo tee /etc/default/transmission-daemon
-echo 'CONFIG_DIR="/home/xbian/.config/transmission-daemon"' | sudo tee -a /etc/default/transmission-daemon
+
+sudo sed -i 's/CONFIG_DIR=.*/CONFIG_DIR=\"\/home\/xbian\/\.config\/transmission\-daemon\"/' /etc/default/transmission-daemon
+cat /etc/default/transmission-daemon | grep -v USER= | sudo tee /etc/default/transmission-daemon
 echo 'USER=xbian' | sudo tee -a /etc/default/transmission-daemon
 sudo sed -i 's/USER=.*/USER=xbian/'  /etc/init.d/transmission-daemon
+sudo sed -i 's/setuid.*/setuid xbian/'  /etc/init/transmission-daemon.conf
+sudo sed -i 's/setgid.*/setgid xbian/'  /etc/init/transmission-daemon.conf
+
 
 mkdir -p /home/xbian/backup_logs
 mkdir -p /home/xbian/backup_scripts
 chmod u+x ./backup_from_wdred4g_to_wdgreen1g.sh
 cp ./backup_from_wdred4g_to_wdgreen1g.sh /home/xbian/backup_scripts
-crontab -l | { cat; echo "0 10-22 * * * /home/xbian/backup_scripts/backup_from_wdred4g_to_wdgreen1g.sh"; } | sort|uniq |crontab -
+crontab -l | { cat; echo "0 10 * * * /home/xbian/backup_scripts/backup_from_wdred4g_to_wdgreen1g.sh"; } | sort|uniq |crontab -
 
-cp ./shares.conf /etc/samba/
+sudo cp ./shares.conf /etc/samba/
 
